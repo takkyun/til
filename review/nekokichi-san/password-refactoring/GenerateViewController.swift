71c9e7c332102960f47f39736b3b33854590e40e
Takuya Otani <takuya.otani@serenebach.net>
2018-10-18 12:42:46 +1300

    refactoring GenerateViewController
    - define PasswordConfiguration instead of individual variables
    - simplify generatePasswords logic
        - branchOfChar no longer required it's now a part of PasswordConfiguration
        - merge generatePasswordFunc and generateNotSamePasswordFunc
        - setup configuration before executing generatePasswords

--- review/nekokichi-san/password-refactoring/GenerateViewController.swift
+++ review/nekokichi-san/password-refactoring/GenerateViewController.swift
@@ -40,45 +40,42 @@ class GenerateViewController: UIViewController {
     @IBOutlet weak var randomChar: UISwitch!
     @IBOutlet weak var chaosChar: UISwitch!
     
+    struct PasswordConfiguration {
+        var numberOfChar: Int = 0
+        var isUsingLowercase = false
+        var isUsingUpperCase = false
+        var isUsingNumber = false
+        var isUsingSymbol = false
+        var allowsDuplication = false
+        var extraSymbols = ""
+        var charset: String {
+            var string = ""
+            if isUsingLowercase {
+                string += "abcdefghijklmnopqrstuvwxyz"
+            }
+            if isUsingUpperCase {
+                string += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            }
+            if isUsingNumber {
+                string += "0123456789"
+            }
+            if isUsingSymbol {
+                string += "!#$%&()=~^|@[;:],./<>?+*}`{"
+            }
+            string += extraSymbols
+            return string
+        }
+    }
+
     //パスワードの数(5個,10個,15個)
     var numberOfPassword = 0
     
     //文字数(8、10、20)
-    var numberOfChar = 0
-    
-    //文字の形態（0:小文字,1:大文字,2:記号）
-    var formChar:[Int:Bool] = [0:false, 1:false, 2:false]
-    
-    //同じ文字を含めるか
-    var permissionSameChar = false
-    
-    
-    //乱数の材料となる文字列の長さ
-    var len = 0
-    
-    //実際に生成される文字列
-    var characters : String = ""
-    
-    //実際に格納される文字列
-    var randomCharacters = ""
-    
-    //実際に保存する文字列
-    var randomPassword = ""
-    
-    
-    //ViewControllerに渡すための配列
-    var passwordArray = [String]()
+    var configuration = PasswordConfiguration()
     
     //UserDefaults
-    let UD = UserDefaults.standard
+    let userDefault = UserDefaults.standard
 
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-    }
-    
-    //戻る
     @IBAction func backButton(_ sender: Any) {
         
         dismiss(animated: true, completion: nil)
@@ -87,312 +84,139 @@ class GenerateViewController: UIViewController {
     
     //5個
     @IBAction func numberOfFive(_ sender: UISwitch) {
-        
         numberOfTen.isOn = false
         numberOfFifteen.isOn = false
-        
-        if sender.isOn == true {
-            numberOfPassword = 5
-        } else {
-            numberOfPassword = 0
-        }
-        
+        numberOfPassword = sender.isOn ? 5 : 0
     }
     
     //10個
     @IBAction func numberOfTen(_ sender: UISwitch) {
-        
         numberOfFive.isOn = false
         numberOfFifteen.isOn = false
-        
-        if sender.isOn == true {
-            numberOfPassword = 10
-        } else {
-            numberOfPassword = 0
-        }
-        
+        numberOfPassword = sender.isOn ? 10 : 0
     }
     
     //15個
     @IBAction func numberOfFifteen(_ sender: UISwitch) {
-        
         numberOfFive.isOn = false
         numberOfTen.isOn = false
-        
-        if sender.isOn == true {
-            numberOfPassword = 15
-        } else {
-            numberOfPassword = 0
-        }
-        
+        numberOfPassword = sender.isOn ? 15 : 0
     }
     
     //８文字
     @IBAction func charEight(_ sender: UISwitch) {
-        
         //10文字と20文字をOFF
         charTen.isOn = false
         charTwenty.isOn = false
-        
-        if sender.isOn == true {
-            numberOfChar = 8
-        } else {
-            numberOfChar = 0
-        }
-        
+        // ---
+        configuration.numberOfChar = sender.isOn ? 8 : 0
     }
     
     //10文字
     @IBAction func charTen(_ sender: UISwitch) {
-        
         //8文字と20文字をOFF
         charEight.isOn = false
         charTwenty.isOn = false
-        
-        if sender.isOn == true {
-            numberOfChar = 10
-        } else {
-            numberOfChar = 0
-        }
-        
+        // ---
+        configuration.numberOfChar = sender.isOn ? 10 : 0
     }
     
     //20文字
     @IBAction func charTwenty(_ sender: UISwitch) {
-        
         //8文字と10文字をOFF
         charEight.isOn = false
         charTen.isOn = false
-        
-        if sender.isOn == true {
-            numberOfChar = 20
-        } else {
-            numberOfChar = 0
-        }
-        
+        // ---
+        configuration.numberOfChar = sender.isOn ? 20 : 0
     }
     
     //小文字
     @IBAction func charSmall(_ sender: UISwitch) {
-        
-        if sender.isOn == true {
-            formChar[0] = true
-        } else {
-            formChar[0] = false
-        }
-        
+        configuration.isUsingLowercase = sender.isOn
     }
     
     //大文字
     @IBAction func charBig(_ sender: UISwitch) {
-        
-        if sender.isOn == true {
-            formChar[1] = true
-        } else {
-            formChar[1] = false
-        }
-        
+        configuration.isUsingUpperCase = sender.isOn
     }
     
     //記号
     @IBAction func charSymbol(_ sender: UISwitch) {
-        
-        if sender.isOn == true {
-            formChar[2] = true
-        } else {
-            formChar[2] = false
-        }
-        
+        configuration.isUsingNumber = sender.isOn
+        configuration.isUsingSymbol = sender.isOn
     }
     
     //同じ文字を含めるか
     @IBAction func charSame(_ sender: UISwitch) {
-        
-        if sender.isOn == true {
-            permissionSameChar = true
-        } else {
-            permissionSameChar = false
-        }
-        
+        configuration.allowsDuplication = sender.isOn
     }
     
     //おまかせ（小文字と大文字を含める）
     @IBAction func randomChar(_ sender: UISwitch) {
-        
-        //文字数
-        numberOfChar = 10
-        
-        //パスワードの数
+        // パスワードの数
         numberOfPassword = 10
-        
-        //含める文字群
-        characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-        
-        //指定された条件を基にパスワードを生成
-        //パスワードを配列に格納していく
-        generatePasswordFunc()
+    
+        // ---
+        configuration.numberOfChar = 10
+        configuration.isUsingLowercase = true
+        configuration.isUsingUpperCase = true
+        configuration.isUsingNumber = true
+        configuration.isUsingSymbol = false
+        configuration.allowsDuplication = true
+        configuration.extraSymbols = ""
+
+        // ---
+        generatePasswords()
         
         dismiss(animated: true, completion: nil)
-        
     }
     
     //カオス（小文字と大文字と記号を含める）
     @IBAction func chaosChar(_ sender: UISwitch) {
-        
-        //文字数
-        numberOfChar = 20
-        
-        //パスワードの数
+        // パスワードの数
         numberOfPassword = 15
         
-        //含める文字
-        characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&()=~^|@[;:],./<>?+*}`{σΣαβγ"
-        
-        //指定された条件を基にパスワードを生成
-        //パスワードを配列に格納していく
-        generatePasswordFunc()
+        // ---
+        configuration.numberOfChar = 20
+        configuration.isUsingLowercase = true
+        configuration.isUsingUpperCase = true
+        configuration.isUsingNumber = true
+        configuration.isUsingSymbol = false
+        configuration.allowsDuplication = true
+        configuration.extraSymbols = "σΣαβγ"
+
+        // ---
+        generatePasswords()
         
         dismiss(animated: true, completion: nil)
-        
     }
     
     //パスワードを生成するUIButton
     @IBAction func generatePassword(_ sender: Any) {
-        
-        //どんな文字を含めたいかを設定
-        branchOfChar()
-        
-        //もし同じ文字を含めたい場合
-        if permissionSameChar == true {
-            
-            //同じ文字が含まれないように生成
-            generatePasswordFunc()
-            
-        //同じ文字を含めたくない場合
-        } else {
-            //普通に生成
-            generateNotSamePasswordFunc()
-            
-        }
+        // ---
+        configuration.extraSymbols = ""
+
+        // ---
+        generatePasswords()
         
         dismiss(animated: true, completion: nil)
     }
     
-    //同じ文字を含める場合のパスワードを生成
-    func generatePasswordFunc() {
-        
-        //パスワードの個数をfor文で生成
-        for _ in 0..<numberOfPassword {
-            
-            //設定した文字数になるまで文字をランダムに生成
-            for _ in 0..<numberOfChar {
-                
-                len = Int(arc4random_uniform(UInt32(characters.count - 1)))
-                
-                //１文字目からlen番目の文字を追加
-                randomCharacters += String(characters[characters.index(characters.startIndex, offsetBy: len)])
-                
+    // パスワードを生成
+    func generatePasswords() {
+        var passwords = [String]()
+        let charset = configuration.charset
+        while passwordArray.count < numberOfPassword {
+            var password = ""
+            while password.count < configuration.numberOfChar {
+                let offset = Int(arc4random_uniform(UInt32(charset.count - 1)))
+                let string = String(charset[charset.index(charset.startIndex, offsetBy: offset)])
+                if configuration.allowsDuplication || !password.contains(string) {
+                    password += string
+                }
             }
-            
-            //配列に含める
-            passwordArray.append(randomCharacters)
-            
-            //初期化
-            randomCharacters = ""
-            
+            passwords.append(password)
         }
-        
-        //ViewControllerに渡すための配列に追加する
-        UD.set(passwordArray, forKey: "passwordArray")
-        
-        //初期化処理
-        passwordArray = [String]()
-        
-        //遷移
-        dismiss(animated: true, completion: nil)
-        
+        userDefault.set(passwords, forKey: "passwordArray")
     }
-    
-    //パスワード生成の関数
-    func generateNotSamePasswordFunc() {
-        
-        //重複チェックを行うために、一旦文字を格納していく
-        var sameCharArray = [String]()
-        
-        //重複チェック後の文字らを格納
-        var notsameCharArray = [String]()
-        
-        for _ in 0..<numberOfPassword {
-            
-            //重複を繰り返し、設定した文字数になるまで繰り返す
-            while (notsameCharArray.count <= numberOfChar) {
-                
-                len = Int(arc4random_uniform(UInt32(characters.count - 1)))
-                
-                //１文字目からlen番目の文字を追加
-                randomCharacters = String(characters[characters.index(characters.startIndex, offsetBy: len)])
-                
-                //生成した乱数文字を配列に格納
-                sameCharArray.append(randomCharacters)
-                
-                //重複チェック
-                let orderSet = NSOrderedSet(array: sameCharArray)
-                
-                //重複のない配列
-                notsameCharArray = orderSet.array as! [String]
-                
-                //更新
-                sameCharArray = notsameCharArray
-                
-            }
-            
-            //配列を結合
-            randomPassword = notsameCharArray.joined()
-            
-            //配列に含める
-            passwordArray.append(randomPassword)
-            
-            //初期化
-            randomCharacters = ""
-            randomPassword = ""
-            sameCharArray = []
-            notsameCharArray = []
-            
-        }
-        
-        //ViewControllerに渡すための配列に追加する
-        UD.set(passwordArray, forKey: "passwordArray")
-        
-        //初期化処理
-        passwordArray = [String]()
-        
-        //遷移
-        dismiss(animated: true, completion: nil)
-        
-    }
-    
-    //文字形態の条件分岐
-    func branchOfChar() {
-        
-        switch (formChar[0],formChar[1],formChar[2]) {
-        case (true, false, false): //小文字
-            characters = "abcdefghijklmnopqrstuvwxyz"
-        case (false, true, false): //大文字
-            characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        case (false, false, true): //記号
-            characters = "0123456789!#$%&()=~^|@[;:],./<>?+*}`{"
-        case (true, true, false): //小文字と大文字
-            characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        case (true, false, true): //小文字と記号
-            characters = "abcdefghijklmnopqrstuvwxyz0123456789!#$%&()=~^|@[;:],./<>?+*}`{"
-        case (false, true, true): //大文字と記号
-            characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&()=~^|@[;:],./<>?+*}`{"
-        case (true, true, true): //小文字と大文字と記号
-            characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&()=~^|@[;:],./<>?+*}`{"
-        default:
-            break
-        }
-        
-    }
-    
 }
